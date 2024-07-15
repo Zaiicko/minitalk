410ecc87fd964d0c5637c43f08f850b3951ee0b2
Mehdi <zaiicko7@proton.me>
2024-07-15 23:46:33 +0200

    Add : byte struct

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: zaiicko <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/12 23:17:32 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/07/13 00:26:30 by zaiicko          ###   ########.fr       */
+/*   Updated: 2024/07/15 23:45:20 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,11 @@
 
 # include "../libft/inc/libft.h"
 # include <signal.h>
+
+typedef struct byte
+{
+	int		i;
+	char	c;
+}		t_byte;
 
 #endif

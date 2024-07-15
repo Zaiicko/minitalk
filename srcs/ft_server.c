da79e841aa6f9b86647505cb3960aa0c9adeb3ac
Mehdi <zaiicko7@proton.me>
2024-07-16 01:39:25 +0200

    Add : fonction for reset the byte struct, printf w/getpid fonction and boucle with pause for wait the signals

--- srcs/ft_server.c
+++ srcs/ft_server.c
@@ -6,14 +6,29 @@
 /*   By: zaiicko <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/13 00:30:46 by zaiicko           #+#    #+#             */
-/*   Updated: 2024/07/13 00:30:50 by zaiicko          ###   ########.fr       */
+/*   Updated: 2024/07/16 01:36:08 by zaiicko          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
-int	main(void)
+void	reset_byte(t_byte *byte)
 {
-	ft_printf("server");
+	byte->i = 0;
+	byte->c = 0;
+}
+
+void	handler(int bit)
+{
+
+}
+
+int	main()
+{
+	ft_printf("THE PID ARE : %d\n", getpid()); 	
+	signal(SIGUSR1, handler);
+	signal(SIGUSR2, handler);
+	while (1)
+		pause();
 	return (0);
 }
